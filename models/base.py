4d16bff97943705ee16980ddb3954a53355e182e
Pavel Ivanov <ivanovpavel031298@gmail.com>
2024-08-25 21:30:54 +0000

    model config fix

--- models/base.py
+++ models/base.py
@@ -1,4 +1,7 @@
 def get_config_from_locals(**kwargs):
     config = kwargs
     config.pop('self')
+    for key in config:
+        if hasattr(config[key],'__name__'):
+            config[key] = config[key].__name__
     return config
